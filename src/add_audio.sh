2b02120c82e9fa3c9fa4b188b58bd3dcfa016560
Sapnesh Naik <sapneshwk@gmail.com>
2017-03-13 20:18:51 +0530

    corrected a typo where ../temp_data was written wrongly as temp_data

--- src/add_audio.sh
+++ src/add_audio.sh
@@ -2,4 +2,4 @@
 #This file takes audio and video from temp_data folder and joins them and then stores the resultant video into
 #the user specififed final location
 
-`ffmpeg -i temp_data/video.mp4 -i temp_data/audio.mp3 -c copy -map 0:0 -map 1:0 -flags +global_header "$1" -y`
+`ffmpeg -i ../temp_data/video.mp4 -i ../temp_data/audio.mp3 -c copy -map 0:0 -map 1:0 -flags +global_header "$1" -y`
