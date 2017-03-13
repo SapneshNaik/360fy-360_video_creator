960a3c11ff7038674d9730f0155766a3951e21f7
Sapnesh Naik <sapneshwk@gmail.com>
2017-03-13 20:42:18 +0530

    added some comments to gdrive.sh

--- gdrive.sh
+++ gdrive.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-filename=`date +"Date_%d-%m_Time_%I-%M%p".tar.gz`
+filename=`date +"git_%d-%m_Time_%I-%M%p".tar.gz`
 echo $filename
+echo " archiving now"
 tar -zcvf "$filename" *
+echo " Uploading now"
+
 gdrive upload --parent 0B8i5vCWNLhOGTF9OYlFESm5XNnc "$filename"
+echo " removing local archive"
 rm $filename
 echo "DONE"
