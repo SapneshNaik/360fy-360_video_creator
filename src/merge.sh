610b686017dc7edf05c2240163a6a9ce7866c076
Sapnesh Naik <sapneshwk@gmail.com>
2017-03-14 20:38:23 +0530

    added code to produce an unscaled video

--- src/merge.sh
+++ src/merge.sh
@@ -8,6 +8,8 @@
 #`ffmpeg -r $framerate -i vid_files/frames/FY%06d.png -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -s $resolution -c:v libx264 -profile:v high444 -refs 16 -crf 0 -preset ultrafast vid_files/video.mp4 -y`
 
 `ffmpeg -r $1 -i ../temp_data/frames/FY%06d.png -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -s $2 -c:v libx264 -profile:v high444 -refs 16  -preset ultrafast ../temp_data/video.mp4 -y`
+`ffmpeg -r $1 -i ../temp_data/frames/FY%06d.png   -c:v libx264 -profile:v high444 -refs 16  -preset ultrafast ../temp_data/video_unscaled.mp4 -y`
+
 #`ffmpeg -framerate $1 -i vid_files/frames/FY%06d.png -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -codec copy -s $resolution vid_files/video.mp4 -y`
 #`ffmpeg -f image2 -r $1 -i vid_files/frames/FY%06d.png -vcodec libx264 -profile:v high444 -refs 16 -crf 0 -preset ultrafast vid_files/frames/FY%06d.png/video.mp4 -y`
 #`ffmpeg -r $framerate  -i vid_files/frames/FY%06d.png -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' -s $resolution -c:v mjpeg -q:v 3 -an vid_files/output.mov`
