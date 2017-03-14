882c8c35705a59eb07a9cfd4b8e193e2fa41984d
Sapnesh Naik <sapneshwk@gmail.com>
2017-03-14 11:28:48 +0530

    generated python code from ui file using pyuic5

--- extra/help_dialog.py
+++ extra/help_dialog.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'help_dialog.ui'
@@ -13,39 +12,66 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.setWindowModality(QtCore.Qt.ApplicationModal)
-        Dialog.resize(379, 212)
-        Dialog.setAutoFillBackground(False)
-        self.textBrowser = QtWidgets.QTextBrowser(Dialog)
-        self.textBrowser.setGeometry(QtCore.QRect(10, 20, 361, 131))
-        self.textBrowser.setAutoFillBackground(True)
-        self.textBrowser.setObjectName("textBrowser")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(190, 160, 141, 41))
+        Dialog.resize(362, 151)
+        Dialog.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        Dialog.setFocusPolicy(QtCore.Qt.NoFocus)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../../../Personal/cd01e9c9b9e961e1c896e5556cb7b159.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(70, 160, 103, 41))
+        icon.addPixmap(QtGui.QPixmap("../resource/logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        Dialog.setLayoutDirection(QtCore.Qt.LeftToRight)
+        Dialog.setAutoFillBackground(False)
+        Dialog.setSizeGripEnabled(False)
+        Dialog.setModal(False)
+        self.docButton = QtWidgets.QPushButton(Dialog)
+        self.docButton.setGeometry(QtCore.QRect(180, 100, 141, 41))
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("../../../Personal/YouTube-icon-full_color.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton_2.setIcon(icon1)
-        self.pushButton_2.setObjectName("pushButton_2")
+        icon1.addPixmap(QtGui.QPixmap("../../../Personal/cd01e9c9b9e961e1c896e5556cb7b159.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.docButton.setIcon(icon1)
+        self.docButton.setObjectName("docButton")
+        self.tubeButton = QtWidgets.QPushButton(Dialog)
+        self.tubeButton.setGeometry(QtCore.QRect(50, 100, 111, 41))
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("../../../Personal/YouTube-icon-full_color.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.tubeButton.setIcon(icon2)
+        self.tubeButton.setObjectName("tubeButton")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 10, 331, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 30, 321, 21))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(20, 50, 221, 20))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(30, 80, 211, 17))
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setItalic(True)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Help"))
-        self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">If you have trouble understanding something please consider reading the </span><span style=\" font-size:14pt; text-decoration: underline;\">documentation</span><span style=\" font-size:14pt;\"> or watching the </span><span style=\" font-size:14pt; text-decoration: underline;\">tutorial videos</span><span style=\" font-size:14pt;\">.</span></p></body></html>"))
-        self.pushButton.setText(_translate("Dialog", "Documentation"))
-        self.pushButton_2.setText(_translate("Dialog", "YouTube"))
+        self.docButton.setText(_translate("Dialog", "Documentation"))
+        self.tubeButton.setText(_translate("Dialog", "YouTube"))
+        self.label.setText(_translate("Dialog", "If you have trouble understanding something"))
+        self.label_2.setText(_translate("Dialog", "please consider reading the documentation"))
+        self.label_3.setText(_translate("Dialog", "or watching the tutorial videos."))
+        self.label_4.setText(_translate("Dialog", "* Internet Charges may apply"))
 
 if __name__ == "__main__":
     import sys
